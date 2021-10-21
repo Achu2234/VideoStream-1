c72ce0bda1c24af631f231ffdf6e2633e0922777
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-10-21 09:16:45 +0530

    fixed

--- lib/helpers/filters.py
+++ lib/helpers/filters.py
@@ -12,7 +12,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 '''
-
 from pyrogram import filters
 from typing import List, Union
 from lib.config import COMMAND_PREFIXES
@@ -24,5 +23,8 @@ def command(commands: Union[str, List[str]]):
 
 private_filters = filters.private & ~ filters.edited \
     & ~ filters.via_bot & ~ filters.forwarded
+public_filters = filters.group & ~ filters.edited & \
+    ~ filters.via_bot & ~ filters.forwarded
+ilters.forwarded
 public_filters = filters.group & ~ filters.edited & \
     ~ filters.via_bot & ~ filters.forwarded
