50591d2b732066700d087d1f9080a79ed00e3b59
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-10-28 12:04:03 +0700

    Update decorators.py

--- lib/helpers/decorators.py
+++ lib/helpers/decorators.py
@@ -1,15 +1,8 @@
-from pyrogram import Client
 from typing import Callable
+from pyrogram import Client
 from pyrogram.types import Message
+from lib.helpers.admins import get_administrators
 from lib.config import SUDO_USERS
-
-
-def sudo_users(func: Callable) -> Callable:
-    async def decorator(client, message):
-        if message.from_user.id in SUDO_USERS:
-            return await func(client, message)
-
-    return decorator
 
 
 def errors(func: Callable) -> Callable:
@@ -18,5 +11,27 @@ def errors(func: Callable) -> Callable:
             return await func(client, message)
         except Exception as e:
             await message.reply(f"{type(e).__name__}: {e}")
+
+    return decorator
+
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
+
+    return decorator
+
+
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
 
     return decorator
